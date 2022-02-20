20d4cb3fa43c86d4546c81a5335fed392947621b
suraj200981 <suraj200981@yahoo.co.uk>
2022-02-20 15:14:51 +0000

    random numbers being pushed to bar chart for bubblesort, app is now also deployed at algvis.surge.sh

--- src/app/bubble-sort/bubble-sort.component.ts
+++ src/app/bubble-sort/bubble-sort.component.ts
@@ -9,43 +9,43 @@ import { Chart } from 'node_modules/chart.js';
 })
 export class BubbleSortComponent implements OnInit {
   
+  arrayToSory = [1,2,3,4,5];
+
   constructor() { }
 
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels = ['0', '1', '2', '3', '4'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: this.arrayToSory, label: 'Series A'},
+  ];
   ngOnInit(): void {
-    const myChart = new Chart("myChart", {
-    type: 'bar',
-    data: {
-        labels: ['0', '1', '2', '3', '4', '5'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
+   
+
+  }
+
+  
+
+
+  generateRandomNumbers(){
+    //generate 5 random numbers and push to arrayToSory
+    this.arrayToSory = [];
+    for(let i = 0; i < 5; i++){
+      this.arrayToSory.push(Math.floor(Math.random() * 10));
+      
     }
-});
+
+    //update barChartData with new data
+    this.barChartData = [
+      {data: this.arrayToSory, label: 'Series A'},
+    ];
+
+    console.log(this.arrayToSory);
+    
   }
 
 }
